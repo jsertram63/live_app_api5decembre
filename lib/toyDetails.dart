6b798e977d9b3b13673592fa66322777c801935f
jsertram63 <jser>
2023-01-02 19:03:31 +0100

    video player

--- lib/toyDetails.dart
+++ lib/toyDetails.dart
@@ -4,14 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:live_app_api_5decembre/image_container.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import 'model/Toy.dart';
 
-class ToyDetailPage extends StatelessWidget {
+class ToyDetailPage extends StatefulWidget {
 
   final Toy toyParse;
 
   const ToyDetailPage({super.key,required this.toyParse});
+
+  @override
+  State<ToyDetailPage> createState() => _ToyDetailPageState();
+}
+
+class _ToyDetailPageState extends State<ToyDetailPage> {
+
+  late YoutubePlayerController _controller;
+
+
+@override
+  void initState() {
+    // TODO: implement initState
+    final videoId = YoutubePlayer.convertUrlToId(widget.toyParse.video);
+    _controller = YoutubePlayerController(
+      initialVideoId: videoId!, 
+      flags: YoutubePlayerFlags(autoPlay: false));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +48,7 @@ class ToyDetailPage extends StatelessWidget {
   Widget _builderBanner(){
     return Container(
       child: Padding(padding:const EdgeInsets.all(15),
-      child: ImageContainer(url: toyParse.img,width: 200,height: 200,),
+      child: ImageContainer(url: widget.toyParse.img,width: 200,height: 200,),
       ),
     );
   }
@@ -37,17 +57,32 @@ class ToyDetailPage extends StatelessWidget {
     return Padding(padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          Text(toyParse.name,
+         
+          Text(widget.toyParse.name,
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
     ),
         Padding(padding: const EdgeInsets.all(10),
-        child: Text(toyParse.description,
+        child: Text(widget.toyParse.description,
         style: TextStyle(fontSize: 16.0,fontWeight:FontWeight.w300)
-        ))
-        ])
+        ),
+      
+        ),
+         Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: YoutubePlayer(controller: _controller,
+           showVideoProgressIndicator: true,
+           bottomActions: [
+            CurrentPosition(),
+            ProgressBar(
+              isExpanded: true,
+            )
+           ],
+           ),
+         )
+        ],
+        
+        )
 
         );
   }
-
-
 }
